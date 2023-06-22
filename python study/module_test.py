f31abbb5db5741e3da61ba31fb27fbe7eff4b623
Plumsg <sunme5599@naver.com>
2023-06-22 17:34:32 +0900

    c

---
+++ python study/module_test.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#모듈(Module)
+# 함수들이 모여있는 파일
+#함수가 기본적으로 가지고 있는 특징 공유
+#모듈을 만들게 되면 파일만 가지고 있어도 모듈안의 함수 사용 가능
+#파이썬 파일(확장자가 py인 파일)로 저장
+
+
+# In[1]:
+
+
+def sum_test(num1,num2):
+    return num1+num2
+
+
+# In[3]:
+
+
+def mul_test(num1,num2):
+    return num1*num2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
